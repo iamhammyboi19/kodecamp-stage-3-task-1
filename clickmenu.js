8a3d2a8ba473a6d6251a01dfb7b03c66f4e392d3
iamhammyboi19 <88974019+iamhammyboi19@users.noreply.github.com>
2021-09-17 00:37:15 +0100

    projectforpractice
    kodecamp-stage-3-task-1

---
+++ clickmenu.js
@@ -0,0 +1,21 @@
+const btnmenuEl = document.querySelector(".header-icon");
+const heroSectionDivEl = document.querySelector(".hero-section-div");
+btnmenuEl.addEventListener("click", function () {
+  heroSectionDivEl.classList.toggle("nav-open");
+});
+
+// const btnCloseEl = document.querySelector(".the-close-icon");
+// const heroSectionRemover = document.querySelector(".hero-sectiom-div");
+// btnCloseEl.addEventListener("click", function () {
+//   heroSectionRemover.classList.remove("nav-open");
+// });
+// btnNavEl.addEventListener("click", function () {
+// headerEl.classList.toggle("nav-open");
+// });
+const myname = "ahmed adekoya";
+console.log(myname);
+// const btnNavEl = document.querySelector(".btn-mobile-nav");
+// const headerEl = document.querySelector(".header");
+// btnNavEl.addEventListener("click", function () {
+// headerEl.classList.toggle("nav-open");
+// });
